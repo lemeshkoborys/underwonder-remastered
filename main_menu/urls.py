1c0fe1616d4a4fddef1468136ca1c95e8ae25440
Borys Lemeshko <lemeshkob@gmail.com>
2018-11-29 21:42:41 +0200

    f

--- main_menu/urls.py
+++ main_menu/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path,
 from .views import render_menu, render_menu_eng
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('', render_menu, name='menu'),
-    path('eng/', render_menu_eng, name='menu-eng')
+    path('eng/', render_menu_eng, name='menu-eng'),
+    path('data/menu', RedirectView.as_view(url='/menu/'), name='redirect-to-menu'),
+    path('data/menu.pdf', RedirectView.as_view(url='/menu/'), name='redirect-to-menu-frompdf')
 ]
